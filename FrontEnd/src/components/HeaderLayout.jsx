1523d1a4aaafa21240213bcf1193beb99ed8e38c
IulianBotnari <iulianbotnari93@gmail.com>
2025-09-10 18:50:55 +0200

    wip header styled

--- FrontEnd/src/components/HeaderLayout.jsx
+++ FrontEnd/src/components/HeaderLayout.jsx
@@ -1,5 +1,7 @@
 import logo from '../assets/NextLogo.png'
 import negoziNextMappa from '../assets/negozi-next-mappa.jpg'
+import searchIcon from '../assets/search-icon.png'
+import style from './componentsStyle/HeaderLayout.module.css'
 
 
 export default function HeaderLayout() {
@@ -7,16 +9,23 @@ export default function HeaderLayout() {
 
 
     return <>
-        <header>
-            <div id="logo_search_bar">
-                <img id="logo" src={logo} alt='logo'></img>
-                <div id='search_bar'></div>
+        <header className={style.header_style}>
+            <div id="logo_search_bar" className={style.logo_search_bar}>
+                <div id='container_logo' className={style.container_logo}>
+                    <img id="logo" src={logo} alt='logo'></img>
+                </div>
+                <div id='search_bar_container' className={style.search_bar_container}>
+                    <input id='input_search_bar' className={style.input_search_bar} type='text' placeholder='Cerca prodotti o marca'></input>
+                    <img className={style.search_icon} src={searchIcon}></img>
+                </div>
             </div>
 
-            <div id="login_logout">
-                <button id="login">Accedi</button>
-                <button id="chart">Carrello</button>
-                <img id="store_map" src={negoziNextMappa} alt='vai a negozi'></img>
+            <div id="login_logout" className={style.login_logout}>
+                <div id='button_container' className={style.button_container}>
+                    <button id="login">Accedi</button>
+                    <button id="chart">Carrello</button>
+                </div>
+                <img id="store_map" src={negoziNextMappa} className={style.store_map} alt='vai a negozi'></img>
             </div>
 
         </header>
