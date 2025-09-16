b960d18e78658e592072338c6529450fb7b2fa95
IulianBotnari <iulianbotnari93@gmail.com>
2025-09-16 12:17:04 +0200

    wip navbar

--- FrontEnd/src/components/NavBar.jsx
+++ FrontEnd/src/components/NavBar.jsx
@@ -15,20 +15,49 @@ export default function NavBar() {
 
         <nav id="nav_bar" className={style.nav_bar}>
             <div id='nav_bar_category' className={style.nav_bar_category}>
-                <div id="computer_tablet_notebook" onMouseLeave={() => setPopUpComputerTabletNoteBook("none")} onMouseOver={() => setPopUpComputerTabletNoteBook("block")}>COMPUTER TABLET NOTEBOOK</div>
-                <div id="consumabile_accessori" onMouseLeave={() => setPopUpConsumabileAccessori("none")} onMouseOver={() => setPopUpConsumabileAccessori("block")}>CONSUMABILE ACCESSORI</div>
-                <div id="hardware_software" onMouseLeave={() => setPopUpHardwareAccessori("none")} onMouseOver={() => setPopUpHardwareAccessori("block")}>HARDWARE SOFTWARE</div>
-                <div id="monitor_stampanti_periferiche" onMouseLeave={() => setPopUpMonitorStampantiPeriferiche("none")} onMouseOver={() => setPopUpMonitorStampantiPeriferiche("block")}>MONITOR STAMPANTI PERIFERICHE</div>
-                <div id="telefonia_werabile" onMouseLeave={() => setPopUpTelefoniaWrable("none")} onMouseOver={() => setPopUpTelefoniaWrable("block")}>TELEFONIA WEARABLE</div>
-                <div id="giochi" onMouseLeave={() => setPopUpGiochi("none")} onMouseOver={() => setPopUpGiochi("block")}>GIOCHI</div>
+                <div id="computer_tablet_notebook" onMouseLeave={() => setPopUpComputerTabletNoteBook("0")} onMouseOver={() => setPopUpComputerTabletNoteBook("1")}>COMPUTER TABLET NOTEBOOK</div>
+                <div id="consumabile_accessori" onMouseLeave={() => setPopUpConsumabileAccessori("0")} onMouseOver={() => setPopUpConsumabileAccessori("1")}>CONSUMABILE ACCESSORI</div>
+                <div id="hardware_software" onMouseLeave={() => setPopUpHardwareAccessori("0")} onMouseOver={() => setPopUpHardwareAccessori("1")}>HARDWARE SOFTWARE</div>
+                <div id="monitor_stampanti_periferiche" onMouseLeave={() => setPopUpMonitorStampantiPeriferiche("0")} onMouseOver={() => setPopUpMonitorStampantiPeriferiche("1")}>MONITOR STAMPANTI PERIFERICHE</div>
+                <div id="telefonia_werabile" onMouseLeave={() => setPopUpTelefoniaWrable("0")} onMouseOver={() => setPopUpTelefoniaWrable("1")}>TELEFONIA WEARABLE</div>
+                <div id="giochi" onMouseLeave={() => setPopUpGiochi("0")} onMouseOver={() => setPopUpGiochi("1")}>GIOCHI</div>
             </div>
         </nav>
-        <div id='popup_computer_tablet_notebook' className={style.popup_computer_tablet_notebook} style={{ display: PopUpComputerTabletNoteBook }} onMouseOver={() => setPopUpComputerTabletNoteBook("block")} onMouseLeave={() => setPopUpComputerTabletNoteBook("none")}></div>
-        <div id='popup_consumabile_accessori' className={style.popup_consumabile_accessori} style={{ display: PopUpConsumabileAccessori }} onMouseOver={() => setPopUpComputerTabletNoteBook("block")} onMouseLeave={() => setPopUpComputerTabletNoteBook("none")}></div>
-        <div id='popup_hardware_software' className={style.popup_computer_tablet_notebook} style={{ display: PopUpHardwareAccessori }} onMouseOver={() => setPopUpComputerTabletNoteBook("block")} onMouseLeave={() => setPopUpComputerTabletNoteBook("none")}></div>
-        <div id='popup_monitor_stampanti_periferiche' className={style.popup_computer_tablet_notebook} style={{ display: PopUpMonitorStampantiPeriferiche }} onMouseOver={() => setPopUpComputerTabletNoteBook("block")} onMouseLeave={() => setPopUpComputerTabletNoteBook("none")}></div>
-        <div id='popup_telefonia_werabile' className={style.popup_computer_tablet_notebook} style={{ display: PopUpTelefoniaWrable }} onMouseOver={() => setPopUpComputerTabletNoteBook("block")} onMouseLeave={() => setPopUpComputerTabletNoteBook("none")}></div>
-        <div id='popup_giochi' className={style.popup_computer_tablet_notebook} style={{ display: PopUpGiochi }} onMouseOver={() => setPopUpComputerTabletNoteBook("block")} onMouseLeave={() => setPopUpComputerTabletNoteBook("none")}></div>
+        <div id='popup_computer_tablet_notebook' className={style.popup_computer_tablet_notebook} style={{ opacity: PopUpComputerTabletNoteBook }} onMouseOver={() => setPopUpComputerTabletNoteBook("1")} onMouseLeave={() => setPopUpComputerTabletNoteBook("0")}>
+            <div id='popup_options' className={style.popup_options}>
+                <h2 id='popup_title'>Computer Tablet NoteBook</h2>
+                <div id='popup_links'>
+                    <div id='popup_col1'>
+                        <a>Accessori Apple</a>
+                        <a>Accessori Tablet</a>
+                        <a>Computer</a>
+                        <a>NoteBook Rigenerati</a>
+                        <a>WorkStation e Server</a>
+                    </div>
+                    <div id='popup_col2'>
+                        <a>Accessori Desktop</a>
+                        <a>App</a>
+                        <a>Microsoft Surface</a>
+                        <a>Tablet</a>
+                    </div>
+                    <div id='popup_col3'>
+                        <a>Accessori NoteBook</a>
+                        <a>Borse, Zaini, etc</a>
+                        <a>NoteBook</a>
+                        <a>WorkStation/Server</a>
+                    </div>
+                </div>
+            </div>
+
+            <div id='popup_adv' className={style.popup_adv}>
+                <h1>dsfsadf</h1>
+            </div>
+        </div>
+        <div id='popup_consumabile_accessori' className={style.popup_consumabile_accessori} style={{ opacity: PopUpConsumabileAccessori }} onMouseOver={() => setPopUpComputerTabletNoteBook("1")} onMouseLeave={() => setPopUpComputerTabletNoteBook("0")}></div>
+        <div id='popup_hardware_software' className={style.popup_computer_tablet_notebook} style={{ opacity: PopUpHardwareAccessori }} onMouseOver={() => setPopUpComputerTabletNoteBook("1")} onMouseLeave={() => setPopUpComputerTabletNoteBook("0")}></div>
+        <div id='popup_monitor_stampanti_periferiche' className={style.popup_computer_tablet_notebook} style={{ opacity: PopUpMonitorStampantiPeriferiche }} onMouseOver={() => setPopUpComputerTabletNoteBook("1")} onMouseLeave={() => setPopUpComputerTabletNoteBook("0")}></div>
+        <div id='popup_telefonia_werabile' className={style.popup_computer_tablet_notebook} style={{ opacity: PopUpTelefoniaWrable }} onMouseOver={() => setPopUpComputerTabletNoteBook("1")} onMouseLeave={() => setPopUpComputerTabletNoteBook("0")}></div>
+        <div id='popup_giochi' className={style.popup_computer_tablet_notebook} style={{ opacity: PopUpGiochi }} onMouseOver={() => setPopUpComputerTabletNoteBook("block")} onMouseLeave={() => setPopUpComputerTabletNoteBook("0")}></div>
 
 
 
