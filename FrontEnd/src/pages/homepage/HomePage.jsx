4992f29ac64e7b1dc83f4480800efbc16fe4004a
IulianBotnari <iulianbotnari93@gmail.com>
2025-09-21 15:53:10 +0200

    fix fixed carousel bug that hind stop self when a specific button was clicked

--- FrontEnd/src/pages/homepage/HomePage.jsx
+++ FrontEnd/src/pages/homepage/HomePage.jsx
@@ -8,33 +8,34 @@ import rtx50HomeAdv from '../../assets/homeadv/rtx50homeadv.jpg'
 import rtxBackToSchoolHomeAdv from '../../assets/homeadv/rtxbacktoschoolhomeadv.jpg'
 import rtx5060homeAdv from '../../assets/homeadv/rtx5060homeadv.jpg'
 import style from './HomePage.module.scss'
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 let carouselStart = true
 
 export default function HomePage() {
     const [homeAdvImg, setHomeAdvImg] = useState('')
-    const [contatore, setContatore] = useState(0)
+    const [contatore, setContatore] = useState(1)
     const advArray = [msiHomeAdv, asusHomeAdv, rtxBackToSchoolHomeAdv, rtx50HomeAdv, rtx5060homeAdv, gigaByteHomeAdv]
-    console.log(carouselStart);
+    const timerRef = useRef(null)
 
 
     function handleButtonClick(id) {
         carouselStart = false
         setHomeAdvImg(advArray[id])
-
+        clearTimeout(timerRef.current)
     }
+
     useEffect(() => {
 
         if (carouselStart == true) {
 
-            setTimeout(() => {
+            timerRef.current = setTimeout(() => {
                 setHomeAdvImg(advArray[contatore])
                 if (contatore == advArray.length - 1) {
                     setContatore(0);
                 } else {
                     setContatore(contatore + 1);
                 }
-            }, 5000)
+            }, 1000)
         }
     }, [homeAdvImg])
 
@@ -45,13 +46,13 @@ export default function HomePage() {
 
         <main>
             <div id="home_adv" className={style.home_adv}>
-                <img key={homeAdvImg} className={style.image} src={homeAdvImg ? homeAdvImg : msiHomeAdv}></img>
+                <img key={homeAdvImg} className={style.image} src={homeAdvImg ? homeAdvImg : advArray[0]}></img>
                 <div id="home_adv_button_container" className={style.home_adv_button_container}>
                     <button id="0" onFocus={(e) => handleButtonClick(e.target.id)}>MSI a 100$ di steam code</button>
                     <button id="1" onFocus={(e) => handleButtonClick(e.target.id)}>ASUS Back to School</button>
                     <button id="2" onFocus={(e) => handleButtonClick(e.target.id)}>Back to School con RTX serie 50</button>
                     <button id="3" onFocus={(e) => handleButtonClick(e.target.id)}>Geforce RTX 50</button>
-                    <button id="4" onFocus={(e) => handleButtonClick(e.target.id)}> Geforce-RTX-5060</button>
+                    <button id="4" onFocus={(e) => handleButtonClick(e.target.id)}>Geforce-RTX-5060</button>
                     <button id="5" onFocus={(e) => handleButtonClick(e.target.id)}>GIGABYTE game bundle</button>
                 </div>
             </div>
